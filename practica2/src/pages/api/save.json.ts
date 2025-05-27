fc8157a42a2f98d4660038687a4358e7112429d1
0smarcit0 Porras <112773178+0smarcit0@users.noreply.github.com>
2025-05-27 05:35:20 -0400

    cancel

--- practica2/src/pages/api/save.json.ts
+++ practica2/src/pages/api/save.json.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from "astro";
-//import fs from "fs"
+import fs from "fs"
 
 
 export const POST: APIRoute= async ({ request }) => {
   let a = await request.json();
-  //fs.writeFileSync("public/glifos.txt",a.input);
-  //fs.writeFileSync("public/salidaglifos.txt",a.output);
+  fs.writeFileSync("public/glifos.txt",a.input);
+  fs.writeFileSync("public/salidaglifos.txt",a.output);
   return new Response(
     JSON.stringify({
       message: "This was a POST!",
